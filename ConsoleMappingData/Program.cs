1d83b75632ff88300b580bd8c68bd639dcf317c9
cutebaby0630 <lovemath0630@gmail.com>
2020-07-09 09:36:29 +0800

    first commit

---
+++ ConsoleMappingData/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Xceed.Wpf.Toolkit;
+
+namespace ConsoleMappingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1.檔案清單列出
+
+            //1.1 讀取檔案名稱存成List("FileName":filename)
+
+            //Step 2.DB Table List
+
+            //2.1 連接DB
+
+            //2.2 讀取相對應DB table 欄位名稱([TableName],[InitialDataFiles])存成List("FileName":TableName, "CSVName":InitialDataFiles)
+
+            //Step 3. 1 + 2 Mapping
+
+            //3.1 將兩個List利用迴圈比對
+
+            //3.2 印出結果
+
+            //Step 4.Export EXCE
+
+
+
+
+        }
+    }
+}
